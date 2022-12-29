ec9db2f2665db29c965bb4b87e0683f832623129
BayuDC <dev.bayudc@gmail.com>
2022-12-29 11:26:04 +0700

    chore: setup prisma and faker

---
+++ basic-express/prisma/seed.js
@@ -0,0 +1,28 @@
+const { PrismaClient } = require('@prisma/client');
+const faker = require('@faker-js/faker').faker;
+const bcrypt = require('bcryptjs');
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.user.create({
+        data: {
+            username: 'admin',
+            password: await bcrypt.hash('admin123', await bcrypt.genSalt()),
+            role: 'admin',
+        },
+    });
+
+    for (let i = 0; i < 10; i++) {
+        await prisma.user.create({
+            data: {
+                username: faker.internet.userName(),
+                password: await bcrypt.hash('password', await bcrypt.genSalt()),
+                role: 'user',
+            },
+        });
+    }
+}
+
+main()
+    .catch(e => console.log(e))
+    .finally(() => prisma.$disconnect());
